b62f2aff7e27ec3f9af50f67ba4966faa3c39afb
Katerina Urizko <93221162+KaterinaUrizko@users.noreply.github.com>
2024-05-24 22:13:59 +0300

    Tasks 1-8

---
+++ HW-1/hw1.js
@@ -0,0 +1,79 @@
+
+
+// Task 2
+// console.log("Urizko");
+
+// Task 3
+
+// let FirstName= "Kate";
+// let LastName = "Urizko";
+// document.write(FirstName +" "+ LastName +"<br>"); 
+
+// FirstName=LastName;
+// document.write(FirstName +" "+ LastName); 
+
+// Task 4
+
+// const address = {
+// street: "Ukrainian Heroes",
+// building: 45,
+// isBlocked: true,
+// flat: null,
+// floor: undefined
+// }
+
+// Task 5
+
+//  let isAdult = confirm("Are you over 18 years?");
+//  console.log (isAdult);
+
+// Task 6
+
+// let FirstName;
+// let LastName;
+// let StudyGroup;
+// let BirthYear;
+// FirstName = "Kate";
+// LastName = "Urizko";
+// StudyGroup = 4;
+// BirthYear = 1978;
+
+// let isMarried
+// isMarried = false;
+
+// console.log (typeof BirthYear, typeof StudyGroup);
+// console.log (typeof isMarried);
+// console.log (typeof FirstName, typeof LastName);
+
+// let age=null;
+// let address;
+// console.log(typeof age);
+// console.log(typeof address);
+
+// Task 7
+
+// let login = prompt("Enter your login");
+// let email = prompt("Enter your email");
+// let password = prompt("Enter your password")
+
+// document.write("Dear "+login+ ", your email is " +email+ ", your password is " +password+ ".");
+
+
+// Task 8
+
+// const secondsPerHour = 60 * 60;
+
+// const secondsPerDay = secondsInHour * 24;
+
+// const secondsPerMonth = secondsInDay * 30;
+
+
+// document.write("Number of seconds in an hour: " + secondsPerHour + "<br>");
+// document.write("Number of seconds in a day: " + secondsPerDay + "<br>");
+// document.write("Number of seconds in a month: " + secondsPerMonth + "<br>");
+
+
+
+
+
+
